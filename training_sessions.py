ce6ca9d0bb82076db0bbfb5591ac479b630fa57d
vmwhoami <vmwhoami@gmail.com>
2025-06-11 13:03:44 +0300

    add delete method

--- training_sessions.py
+++ training_sessions.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from pydantic import BaseModel, Field
 
 app = FastAPI()
-
 class Training:
     id: int
     name: str  
@@ -19,9 +18,8 @@ class Training:
         self.weight = weight
         self.notes = notes
         
-
 class TrainingRequest(BaseModel):
-    id: int = Field(description='ID is not neede on create', default=None) 
+    id: int
     name: str=Field(min_length=3)
     sets: int
     reps: int
@@ -71,7 +69,6 @@ def training_by_name(name: str):
             training_to_return.append(training)
         return training_to_return
     
-
 @app.post("/training_sessions/create_session")
 async def create_new_training(session_request: TrainingRequest):
         new_session = Training(**session_request.model_dump())
@@ -86,3 +83,18 @@ def find_training_id(training: Training):
     #     training.id = 1
         
     return training
+
+@app.put("/training_sessions/update_training_sessions")
+async def update_training_session(session_request: TrainingRequest):
+    for t in range(len(TRAINING_SESSIONS)):
+        if TRAINING_SESSIONS[t].id == session_request.id:
+            TRAINING_SESSIONS[t] = session_request
+
+
+@app.delete("/training_sessions/{session_id}")
+async def delete_training_session(session_id: int):
+    for t in range(len(TRAINING_SESSIONS)):
+        if TRAINING_SESSIONS[t].id == session_id:
+            TRAINING_SESSIONS.pop(t)
+            break
+        
