24673cb4421b9cf6482d87c75dded872e7c3b1a2
vmwhoami <vmwhoami@gmail.com>
2025-06-12 11:55:43 +0300

    finish sattus codes

--- training_sessions.py
+++ training_sessions.py
@@ -1,6 +1,7 @@
 from datetime import datetime
-from fastapi import FastAPI
-from pydantic import BaseModel, Field
+from fastapi import FastAPI, Path, Query, HTTPException
+from pydantic import BaseModel, Field, ConfigDict
+from starlette import status
 
 app = FastAPI()
 class Training:
@@ -12,7 +13,7 @@ class Training:
     notes: str
     session_date: datetime
 
-    def __init__(self, id,name, sets, reps, weight, notes):
+    def __init__(self, id,name, sets, reps, weight, notes, session_date):
         self.id = id
         self.name = name 
         self.sets = sets
@@ -39,34 +40,35 @@ class TrainingRequest(BaseModel):
                 "reps": 2,
                 "weight": 10,
                 "notes": "Some notes about the sesion",
-                "session_date": ""
+                "session_date": "2025-06-12T15:30:00Z"
             }
         }
     }
 
 TRAINING_SESSIONS = [
-    Training(1, "Lat Pulldown", 3, 2, 89.52, "pull down", "!!!!!!!!date"),
-    Training(2, "Bench Press", 4, 3, 102.34, "push"),
-    Training(3, "Deadlift", 3, 2, 140.25, "lift"),
-    Training(4, "Squat", 5, 3, 120.78, "lower"),
-    Training(5, "Overhead Press", 3, 2, 75.60, "press"),
-    Training(6, "Barbell Row", 4, 3, 85.90, "pull"),
-    Training(7, "Leg Curl", 3, 2, 65.30, "curl"),
-    Training(8, "Bicep Curl", 3, 2, 45.80, "curl")
+    Training(1, "Lat Pulldown", 3, 2, 89.52, "pull down", "2025-03-12T15:30:00Z"),
+    Training(2, "Bench Press", 4, 3, 102.34, "push","2025-02-12T15:30:00Z" ),
+    Training(3, "Deadlift", 3, 2, 140.25, "lift","2022-10-12T15:30:00Z" ),
+    Training(4, "Squat", 5, 3, 120.78, "lower","2023-11-12T15:30:00Z"),
+    Training(5, "Overhead Press", 3, 2, 75.60, "press","2023-11-12T15:30:00Z"),
+    Training(6, "Barbell Row", 4, 3, 85.90, "pull","2022-11-12T13:00:00Z"),
+    Training(7, "Leg Curl", 3, 2, 65.30, "curl","2023-11-12T15:00:00Z"),
+    Training(8, "Bicep Curl", 3, 2, 45.80, "curl","2025-01-12T15:30:00Z")
 ]
 
-@app.get("/training_sessions")
+@app.get("/training_sessions", status_code=status.HTTP_200_OK)
 def training_sessions():
     return TRAINING_SESSIONS
 
 
-@app.get("/training_sessions/{training_sersion_id}")
+@app.get("/training_sessions/{training_sersion_id}", status_code=status.HTTP_200_OK)
 def training_sessions_by_id(training_sersion_id: int):
     for training in TRAINING_SESSIONS:
         if training.id == training_sersion_id:
             return training
+    raise HTTPException(status_code=404, detail="Training not found")
         
-@app.get("/training_sessions/")
+@app.get("/training_sessions/",status_code=status.HTTP_200_OK)
 def training_by_name(name: str):
     training_to_return = []
     for training in TRAINING_SESSIONS:
@@ -74,7 +76,7 @@ def training_by_name(name: str):
             training_to_return.append(training)
         return training_to_return
     
-@app.post("/training_sessions/create_session")
+@app.post("/training_sessions/create_session", status_code=status.HTTP_201_CREATED)
 async def create_new_training(session_request: TrainingRequest):
         new_session = Training(**session_request.model_dump())
         TRAINING_SESSIONS.append(find_training_id(new_session))
@@ -89,17 +91,24 @@ def find_training_id(training: Training):
         
     return training
 
-@app.put("/training_sessions/update_training_sessions")
+@app.put("/training_sessions/update_training_sessions", status_code=status.HTTP_204_NO_CONTENT)
 async def update_training_session(session_request: TrainingRequest):
+    training_session_changed = False
     for t in range(len(TRAINING_SESSIONS)):
         if TRAINING_SESSIONS[t].id == session_request.id:
             TRAINING_SESSIONS[t] = session_request
+            training_session_changed = True
+    if not training_session_changed:
+        raise HTTPException(status_code=404, detail="Training not found")
 
 
-@app.delete("/training_sessions/{session_id}")
+@app.delete("/training_sessions/{session_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_training_session(session_id: int):
+    training_session_changed = False
     for t in range(len(TRAINING_SESSIONS)):
         if TRAINING_SESSIONS[t].id == session_id:
             TRAINING_SESSIONS.pop(t)
+            training_session_changed = True
             break
-        
+    if not training_session_changed:
+        raise HTTPException(status_code=404, detail="Training not found")
